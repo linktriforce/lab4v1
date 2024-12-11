a545133ee7ea0b45946d452ad9c5a3720a961863
linktriforce <54026512+linktriforce@users.noreply.github.com>
2024-12-11 12:53:01 +0100

    uppgift 1

--- tag_remover.cc
+++ tag_remover.cc
@@ -7,7 +7,14 @@
 TagRemover::TagRemover(std::istream &is) {
     // Read from the input stream (for example, cin)
     std::string line;
-    while (std::getline(is, line)) {
+    while (std::getline(is, line) && line != "exit") { 
+        auto start_pos = line.find_first_of('<');
+        auto end_pos = line.find_last_of('>');
+        if (end_pos != std::string::npos) {
+            line = line.substr(start_pos + 1, end_pos - 1);
+        }
+        //Find special characters 
+
         content += line + "\n";  // Store the input in a member variable (content)
     }
 }
@@ -17,8 +24,30 @@ void TagRemover::print(std::ostream &os) {
     os << content;
 }
 
+
+//Write a class that reads a file and removes HTML tags and 
+// translates HTML-encoded special characters.
 int main() {
     TagRemover tr(std::cin);   // read from cin
     tr.print(std::cout);       // print on cout
     return 0;
 }
+
+/* Example input
+<html>
+<head>
+    <title>Test Page</title>
+</head>
+<body>
+    <h1>Welcome to &lt;MySite&gt;</h1>
+    <p>This is a &quot;test&quot; of the TagRemover class.</p>
+    <p>Use &amp; for ampersand and &nbsp; for space.</p>
+</body>
+</html>
+*/
+
+/* Output
+Welcome to <MySite>
+This is a "test" of the TagRemover class.
+Use & for ampersand and   for space.
+*/
